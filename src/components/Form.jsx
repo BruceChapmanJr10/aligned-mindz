376801f17d5b92b467d6e56a91dac5302b520c56
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2025-03-05 21:10:23 -0500

    unistalled million

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 
 const Form = () => {
+  function handleFormData(formData) {
+  
+    const name = formData.get("user-name");
+    const email = formData.get("user-email");
+    const address = formData.get("user-address");
+    const message = formData.get("message");
+
+    console.log(name, email, address, message);
+  }
   return (
     <div id="contact" className="  bg-purple-200 bg-cover pb-5">
       <h1 className="text-orange-700 pt-9 text-center font-primary text-4xl font-bold py-9">
         Contact
       </h1>
-      <form className=" bg-purple-950 space-y-4 pt-3 max-w-2xl lg:w-lg m-auto  rounded-2xl">
+      <form
+        action={handleFormData}
+        className=" bg-purple-950 space-y-4 pt-3 max-w-2xl lg:w-lg m-auto  rounded-2xl"
+      >
         <div>
           <label htmlFor="name" className="block text-white ml-3 mb-2">
             Name
           </label>
           <input
-            name="user_name"
+            name="user-name"
             type="text"
             className="w-full pt-2 rounded bg-gray-300 border border-gray-600 focus:outline-none focus:border-purple-400"
             placeholder="Enter Your Name"
@@ -25,7 +37,7 @@ const Form = () => {
             Email
           </label>
           <input
-            name="user_email"
+            name="user-email"
             type="email"
             className="w-full pt-2 rounded bg-gray-300 border border-gray-600 focus:outline-none focus:border-purple-400"
             placeholder="Enter Your Email"
@@ -37,7 +49,7 @@ const Form = () => {
             Address
           </label>
           <input
-            name="user_address"
+            name="user-address"
             type="text"
             className="w-full pt-2 rounded bg-gray-300 border border-gray-600 focus:outline-none focus:border-green-400"
             placeholder="Enter Your Address"
@@ -53,11 +65,11 @@ const Form = () => {
             className="w-full pt-2 rounded bg-gray-300 border border-gray-600 focus:outline-none focus:border-green-400"
             placeholder="How can I help you..."
             rows="5"
+            name="message"
           />
         </div>
         <div className="flex justify-center pb-5">
           <button
-            type="submit"
             className="cursor-pointer bg-gradient-to-r from-purple-500 to-orange-800 text-white 
                md:inline transform transition-transform duration-300 hover:scale-105 px-6 py-2 rounded-full "
           >
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import MillionLint from "@million/lint";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    MillionLint.vite({
-      enabled: true,
-    }),
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   base: "/aligned-mindz",
 });
